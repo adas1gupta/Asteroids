85705c19fc714cc4d17e93fb7560c11871931689
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-02-27 17:40:05 -0500

    fps rate limiting introduced

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -13,9 +15,7 @@ def main():
         screen.fill((0, 0, 0)) #must be in RGB
         pygame.display.flip()
     
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
