2c0aaeaf73698da058a6231616170ad9ca70ee46
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-02-27 18:41:50 -0500

    implemented player shape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player #for some reason, import player isn't enough
 from constants import *
 
 def main():
@@ -6,16 +7,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0, 0, 0)) #must be in RGB
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-    
-        dt = clock.tick(60) / 1000
+        
+        dt = clock.tick(60) / 1000 #limit framerate
 
 if __name__ == "__main__":
     main()
