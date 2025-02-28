92f48a8518d989709a8a08e1701c9b558ad636e5
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-02-28 11:41:00 -0500

    asteroids implemented

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from player import Player #for some reason, import player isn't enough
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 
 def main():
@@ -9,6 +11,11 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
     Player.containers = (updatable, drawable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
