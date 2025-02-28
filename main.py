9e5957616b52c898fb4fca5e9a1d5dcf36b1835e
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-02-27 19:02:08 -0500

    rotate method implemented for A and D of the WASD move commands

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ def main():
         
         screen.fill("black")
         player.draw(screen)
+        
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000 #limit framerate
+        player.update(dt)
 
 if __name__ == "__main__":
     main()
