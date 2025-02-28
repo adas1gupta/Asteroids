9a17ac401c4c69512723e37db94a62756c356951
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-02-27 19:29:44 -0500

    added containers for objects

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ from constants import *
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
         
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000 #limit framerate
-        player.update(dt)
+        
 
 if __name__ == "__main__":
     main()
